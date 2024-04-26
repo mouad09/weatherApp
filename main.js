b0033f0d81f68dace8633b1f68dea1eee777ee07
mouad09 <mouadessabouni@gmail.com>
2024-04-26 16:57:02 +0100

    les valeurs

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ let cityInput  = document.getElementById("city-input"),
 seatchBtn    = document.getElementById('searchBtn'),
 api_key = '3afd4977adc22ca910dc667a7499c45f' ,
 currentWeatherCard = document.querySelectorAll('.weather-left .card')[0],
-fiveDaysForecastCard = document.querySelector('.day-forecast');
+fiveDaysForecastCard = document.getElementById('dayForecast');
+aqiCard = document.querySelectorAll('.highlights .card')[0],
+aqiList = ['Good', 'Fair', 'Moderate','Poor','Very Poor'];
 
 function getWeatherDetails(name , lat , lon , country , state){
     let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`,
     WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,
+    AIR_POLLUTION_API_URL = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${api_key}`,
     days = [
        "Sunday" , 
        "Monday" ,   
@@ -30,6 +33,57 @@ function getWeatherDetails(name , lat , lon , country , state){
         'nov' ,
         'dec'
     ];
+      
+     fetch(AIR_POLLUTION_API_URL).then(res=> res.json()).then(data => {
+        let {co, no, no2, o3, so2, pm2_5, pm10, nh3} = data.list[0].components;
+          aqiCard.innerHTML = `
+          <div class="card">
+              <div class="card-head">
+              <p>Air Quality Index</p>
+              <p class="air-index aqi-${data.list[0].main.aqi}">${aqiList[data.list[0].main.aqi - 1]}</p>
+              </div>
+         <div class="air-indices">
+              <i class="fa-regular fa-wind fa-3x"></i>
+              <div class="item">
+                  <p>PM2.5</p>
+                  <h2>${pm2_5}</h2>
+              </div>
+              <div class="item">
+                  <p>PM10</p>
+                  <h2>${pm10}</h2>
+              </div>
+              <div class="item">
+                  <p>SO2</p>
+                  <h2>${so2}</h2>
+              </div>
+              <div class="item">
+                  <p>CO</p>
+                  <h2>${co}</h2>
+              </div>
+              <div class="item">
+                  <p>NO</p>
+                  <h2>${no}</h2>
+              </div>
+              <div class="item">
+                  <p>NO2</p>
+                  <h2>${no2}</h2>
+              </div>
+              <div class="item">
+                  <p>NH3</p>
+                  <h2>${nh3}</h2>
+              </div>
+              <div class="item">
+                  <p>O3</p>
+                  <h2>${o3}</h2>
+              </div>
+          </div>
+          `;
+
+     }).catch(() => { 
+        alert('Failed to fetch Air  Quality data!') ;
+     });
+
+
     fetch(WEATHER_API_URL).then((res) => res.json()).then(data=>{
        let date = new Date();
         currentWeatherCard.innerHTML = `
@@ -61,21 +115,38 @@ function getWeatherDetails(name , lat , lon , country , state){
                 return uniqueForecastDays.push(forecastDate);
             }
         });
-        // console.log(fiveDaysForecast);
+        // console.log(fiveDaysForecast.lenght);
         fiveDaysForecastCard.innerHTML = '';
-        for( i = 1 ; i < fiveDaysForecast.lenght; i++){
-            let date = new Date(fiveDaysForecast[i].dt_txt);
-            fiveDaysForecastCard.innerHTML += `
-            <div class="forecast-item">
-            <div class="icon-wrapper">
-                <img src="https://openweathermap.org/img/wn/${fiveDaysForecast[i].weather[0].icon}.png">
-                <span>${(fiveDaysForecast[i].main.temp - 273.15).toFixed(2)}&deg;C</span>
-            </div>
-            <p>${date.getDate()} ${months[date.getMonth()]}</p>
-            <p>${days[date.getDay()]}:</p>
-            </div>
-            `;
-        }
+        // for( i = 1 ; i < fiveDaysForecast.lenght; i++){
+        //     let date = new Date(fiveDaysForecast[i].dt_txt);
+        //     fiveDaysForecastCard.innerHTML = `
+        //     <div class="forecast-item">
+        //     <div class="icon-wrapper">
+        //         <img src="https://openweathermap.org/img/wn/${fiveDaysForecast[i].weather[0].icon}.png">
+        //         <span>${(fiveDaysForecast[i].main.temp - 273.15).toFixed(2)}&deg;C</span>
+        //     </div>
+        //     <p>${date.getDate()} ${months[date.getMonth()]}</p>
+        //     <p>${days[date.getDay()]}:</p>
+        //     </div> `;
+       
+            // console.log(fiveDaysForecast); 
+        // }
+        fiveDaysForecast.forEach((forecast, i) => {
+            if (i !== 0) {
+                let date = new Date(forecast.dt_txt);
+                let forecastItem = document.createElement('div');
+                forecastItem.classList.add('forecast-item');
+                forecastItem.innerHTML = `
+                    <div class="icon-wrapper">
+                        <img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png">
+                        <span>${(forecast.main.temp - 273.15).toFixed(2)}&deg;C</span>
+                    </div>
+                    <p>${date.getDate()} ${months[date.getMonth()]}</p>
+                    <p>${days[date.getDay()]}:</p>
+                `;
+                fiveDaysForecastCard.appendChild(forecastItem);
+            }
+        });
     }).catch(()=> {
         alert('Failed to  load forecast data');
     });
